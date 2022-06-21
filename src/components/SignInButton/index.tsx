8e1b3a0641f7e99083eed7f8b5ac4c2c338e3e96
Cardoso <cardosoassuncao@gmail.com>
2022-06-21 13:14:36 -0400

    save data of event

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,6 +2,7 @@ import { signIn, signOut, useSession } from "next-auth/react"
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
+
 export function SignInButton() {
   const { data: session, status } = useSession()
   return session ? (
